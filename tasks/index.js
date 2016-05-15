f0da50a1bd2ebfc3508af24458cc44d1e9cbd634
Luca Barone <baro.luc@gmail.com>
2016-05-15 12:18:55 +0200

    add images task

--- tasks/index.js
+++ tasks/index.js
@@ -2,5 +2,5 @@ import gulp from 'gulp'
 import seq from 'gulp-sequence'
 
 gulp.task('default', seq('build', 'watch'))
-gulp.task('build', seq('clean', ['html', 'css']))
-gulp.task('watch', seq('sync', ['html:watch', 'css:watch']))
+gulp.task('build', seq('clean', ['html', 'css', 'images']))
+gulp.task('watch', seq('sync', ['html:watch', 'css:watch', 'images:watch']))
